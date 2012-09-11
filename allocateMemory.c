49e6e3e850392e9db569a2e0e1db975f2e4c5c88
chester <chester@chester>
2012-09-11 17:17:07 +0800

    To malloc memory from **(to_level_pointer)

---
+++ allocateMemory.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<malloc.h>
+#include <strings.h>
+#include <string.h>
+
+void AllocateMemory(char **, int, char *); 
+
+int main(void){
+	char *s = 0;
+	char *d = "test";
+	int l = strlen(d);
+
+	AllocateMemory(&s,l,d);
+	/*  
+	 *      int l = strlen(d);
+	 *          
+	 *		s = malloc(l + 1);
+	 *      bzero(s, l + 1);
+	 *      memcpy(s, d, l);
+	 *
+	 */
+
+	printf("now *s is %s\n", s); 
+}
+
+
+void AllocateMemory(char **s, int l, char *d) 
+{
+	*s = malloc(l + 1); 
+	bzero(*s, l + 1); 
+	memcpy(*s, d, l); 
+}

660ac607d4711a1295f549132b95db43dbf4ac91
tetang1230 <tetang1230@gmail.com>
2014-12-08 14:05:17 +0800

    试验野指针

---
+++ practice/malloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+/**
+ *
+ * 试验野指针
+ *
+ */
+int main(int argc, const char *argv[])
+{
+    char * p = (char *)malloc(10*sizeof(char));
+
+    free(p);
+
+    p[100] = 'a';
+    p--;
+
+    p[0] = 'c';
+
+    printf("p is %s\n", p);
+
+
+    return 0;
+}
